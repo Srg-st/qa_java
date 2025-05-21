7915e1b80807fef781fce5f0eb52c2737dd593ca
Sergey Stepanov <srgst11@yandex.ru>
2025-05-21 14:52:55 +0300

    Исправлены критические замечания ревью, также учтены некоторые комментарии

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,25 +12,31 @@ import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
+    private Cat cat;
 
     @Mock
     Feline felineMock;
 
+    @Before
+    public void initCat(){
+        cat = new Cat (felineMock);
+    }
+
     @Test
     public void testCatGetSound() {
-        Cat cat = new Cat (felineMock);
-
         Assert.assertEquals("Мяу", cat.getSound());
     }
 
     @Test
     public void testCatGetFood() throws Exception {
-        Cat cat = new Cat (felineMock);
         Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Хищник"));
-
         Assert.assertEquals(List.of("Хищник"), cat.getFood());
-
+    }
+    //Выделил отдельно тест на проверку количества вызовов метода eatMeat()
+    @Test
+    public void testCatGetFoodNumberInvoc() throws Exception {
+        Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Хищник"));
+        cat.getFood();
         Mockito.verify(felineMock, Mockito.times(1)).eatMeat();
-
     }
 }
--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -3,7 +3,6 @@ package com.example;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -13,28 +12,25 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
 
-    @Mock
-    private Feline felineMock;
+    // Вместо мока используется шпион для класса Feline
+    @Spy
+    private Feline felineSpy = new Feline();
 
     @Test
     public void testEatMeat() throws Exception {
-        Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        Feline feline = new Feline();
-        List<String> actualEatMeat = feline.eatMeat();
-        Assert.assertEquals(felineMock.eatMeat(), actualEatMeat);
+        Mockito.when(felineSpy.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actualEatMeat = felineSpy.eatMeat();
+        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), actualEatMeat);
     }
 
     @Test
     public void testGetFamily() throws Exception {
-        Mockito.when(felineMock.getFamily()).thenReturn("Кошачьи");
-
+        Mockito.when(felineSpy.getFamily()).thenReturn("Кошачьи");
         Feline feline = new Feline();
         String actualGetFamily = feline.getFamily();
-        Assert.assertEquals(felineMock.getFamily(), actualGetFamily);
+        Assert.assertEquals(felineSpy.getFamily(), actualGetFamily);
     }
 
-    @Spy
-    private Feline felineSpy;
 
     @Test
     public void testGetKittens() throws Exception {
@@ -45,9 +41,14 @@ public class FelineTest {
     @Test
     public void testGetKittensNoParams() throws Exception {
         Mockito.when(felineSpy.getKittens()).thenReturn(1);
-        Mockito.verify(felineSpy, Mockito.times(1)).getKittens();
         int actualResult = felineSpy.getKittens();
         Assert.assertEquals(1, actualResult);
+    }
+    //Выделил отдельно тест для проверки количества вызовов метода getKittens()
+    @Test
+    public void testGetKittensNoParamsNumberInvoc() throws Exception {
+        felineSpy.getKittens();
+        Mockito.verify(felineSpy, Mockito.times(1)).getKittens();
     }
 
 
